88867bbf6f4d613b308586f0c51721edf7fe1045
ednaldomartins <martinsednaldo@outlook.com>
2019-06-23 13:39:52 -0300

    adição de comentários, validação de valores, e separação de métodos.

--- root/js/questao-1.js
+++ root/js/questao-1.js
@@ -1,10 +1,31 @@
 function calcular() {
-    var output = document.querySelector("#output");
+    //recebe os valores
     n1 = parseInt(prompt("n1"));
     n2 = parseInt(prompt("n2"));
 
-    output.innerHTML += "n1 + n2 |" + (n1+n2) + "<p>";
-    output.innerHTML += "n1 * n2 |" + (n1*n2) + "<p>";
-    output.innerHTML += "n1 / n2 |" + (n1/n2) + "<p>";
-    output.innerHTML += "n1 % n2 |" + (n1%n2) + "<p>";
+    //validar valores recebidos pelo usuário
+    try {
+        validarValores(n1, n2);
+        //selecionar a <div> q vai ser escrita, e escrever
+        var output = document.querySelector("#output");    
+        escrever(output, n1, n2)
+    } catch(e) {
+        alert(e);
+    }   
 }   
+
+function validarValores(n1, n2) {
+    //lancar throw
+    if(isNaN(n1) | isNaN(n2)) 
+        throw new Error("Digite apenas números");
+}
+
+function escrever(output, n1, n2) {
+    //limpar texto anterior
+    output.innerHTML = "";
+    //escrever na <div> output
+    output.innerHTML += n1 + " + " + n2 + " | " + (n1+n2) + "<p>" +
+                        n1 + " * " + n2 + " | " + (n1*n2) + "<p>" +
+                        n1 + " / " + n2 + " | " + (n1/n2) + "<p>" +
+                        n1 + " % " + n2 + " | " + (n1%n2) + "<p>";
+}
