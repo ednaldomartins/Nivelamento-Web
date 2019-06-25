0e3f5ffe8ef8f77575b4b5f437e3cc12195af159
ednaldomartins <martinsednaldo@outlook.com>
2019-06-25 10:52:10 -0300

    As variáveis que guardam as referências das query agora são globais.

--- root/js/questao-1.js
+++ root/js/questao-1.js
@@ -1,55 +1,42 @@
+// selecionar o que vai ser utilizado
+var corpoTabela = document.querySelector('#corpoTabela');
+var templateTabela = document.querySelector('#templateTabela');
+var listaTd = templateTabela.content.querySelectorAll('td');
 
 function calcular() {
-    //recebe os valores
-    var n1 = parseFloat(document.querySelector("#n1").value);
-    var n2 = parseFloat(document.querySelector("#n2").value);
+	//recebe os valores
+	var n1 = parseFloat(document.querySelector('#n1').value);
+	var n2 = parseFloat(document.querySelector('#n2').value);
 
-    //validar valores recebidos pelo usuário
-    try {
-        validarValores(n1, n2);   
-        //contruindo tabela
-        escrever(n1, n2);
-    } catch(e) {
-        alert(e);
-    }   
-}   
+	//validar valores recebidos pelo usuário e criar tabela
+	try {
+		validarValores(n1, n2);
+		criarTabela(n1, n2);
+	} catch (e) {
+		alert(e);
+	}
+}
 
 function validarValores(n1, n2) {
-    //lancar throw
-    if(isNaN(n1) | isNaN(n2)) 
-        throw "Aviso: Digite o outro número."
+	//lancar throw
+	if (isNaN(n1) | isNaN(n2)) throw 'Aviso: Digite o outro número.';
 }
 
-function escrever(n1, n2) {
-    // ? jeito errado de apagar o corpo da tabela ?
-    document.querySelector("#corpoTabela").innerHTML = "";
+function criarTabela(n1, n2) {
+	// ? jeito errado de apagar o corpo da tabela ?
+	corpoTabela.innerHTML = '';
 
-    //addLinha(_, truncar tudo apos 3 casas decimais)
-    adicionarLinha( (n1 + " + " + n2), (parseInt((n1+n2)*1000)/1000)  );
-    adicionarLinha( (n1 + " * " + n2), (parseInt((n1*n2)*1000)/1000)  );
-    adicionarLinha( (n1 + " / " + n2), (parseInt((n1/n2)*1000)/1000)  );
-    adicionarLinha( (n1 + " % " + n2), (parseInt((n1%n2)*1000)/1000) );
+	//addLinha(_, truncar tudo apos 3 casas decimais)
+	adicionarLinha(n1 + ' + ' + n2, parseInt((n1 + n2) * 1000) / 1000);
+	adicionarLinha(n1 + ' * ' + n2, parseInt(n1 * n2 * 1000) / 1000);
+	adicionarLinha(n1 + ' / ' + n2, parseInt(n1 / n2 * 1000) / 1000);
+	adicionarLinha(n1 + ' % ' + n2, parseInt((n1 % n2) * 1000) / 1000);
 }
 
-function escreverDiv(output, n1, n2) {
-    //limpar texto anterior
-    output.innerHTML = "";
-    //escrever na <div> output
-    output.innerHTML += n1 + " + " + n2 + " | " + (n1+n2) + "<p>" +
-                        n1 + " * " + n2 + " | " + (n1*n2) + "<p>" +
-                        n1 + " / " + n2 + " | " + (n1/n2) + "<p>" +
-                        n1 + " % " + n2 + " | " + (n1%n2) + "<p>";
-}
-
-function adicionarLinha( n1, n2) {
-    // selecionar o que vai ser utilizado
-    var corpoTabela = document.querySelector("#corpoTabela");
-    var templateTabela = document.querySelector("#templateTabela");
-    var listaTd = templateTabela.content.querySelectorAll("td");
-
-    //criar linha e adicionar ao corpo
-    listaTd[0].textContent = n1;
-    listaTd[1].textContent = n2;
-    var novaLinha = document.importNode(templateTabela.content, true);
-    corpoTabela.appendChild(novaLinha);
+function adicionarLinha(n1, n2) {
+	//criar linha e adicionar ao corpo
+	listaTd[0].textContent = n1;
+	listaTd[1].textContent = n2;
+	var novaLinha = document.importNode(templateTabela.content, true);
+	corpoTabela.appendChild(novaLinha);
 }
