1e2305e2551bef18aaa7c46799c84e17a6992acf
ednaldomartins <martinsednaldo@outlook.com>
2019-06-28 12:44:22 -0300

    chamada de funções da calculadora.

---
+++ root/js/calculadora.js
@@ -0,0 +1,22 @@
+function adicionarNumero(n) {
+	var numero = document.querySelector('#numero');
+	numero.innerHTML += n;
+}
+
+function adicao() {}
+
+function subtracao() {}
+
+function multiplicacao() {}
+
+function divisao() {}
+
+function igual() {}
+
+function ponto() {}
+
+function limparDisplay() {}
+
+function apagarDigito() {}
+
+function porcentagem() {}
