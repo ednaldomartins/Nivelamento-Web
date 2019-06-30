a7aaa4cbaeb9300f7534b50ecf4f0691d633e7b7
ednaldomartins <martinsednaldo@outlook.com>
2019-06-30 12:20:36 -0300

    Implementação das ações básicas para funcionamento da calculadora.

--- root/js/calculadora.js
+++ root/js/calculadora.js
@@ -42,10 +42,40 @@ function igual() {
 	resultadoAtual = 0;
 }
 
-function ponto() {}
+/**
+ * Link abaixo explicando como utilizar RegExp() em javascript.
+ * ! link: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Guide/Regular_Expressions
+ */
+function ponto() {
+	if (numero.innerHTML.search('\\.') === -1) numero.innerHTML += '.';
+}
 
-function limparDisplay() {}
+function limparDisplay() {
+	numero.innerHTML = 0;
+}
 
-function apagarDigito() {}
+function apagarDigito() {
+	if (numero.textContent.length > 1) {
+		numero.innerHTML = numero.textContent.substring(0, numero.textContent.length - 1);
+	} else {
+		numero.innerHTML = 0;
+	}
+}
 
-function porcentagem() {}
+function porcentagem() {
+	//! A forma de calcular a porcentagem varia de acordo com o sinal da operacao.
+	/**
+	 * Quando +|- recalculamos o valor guardado aplicando um novo baseado na porcentagem
+	 * exemplo: 950 + 10% = 1045, 950 - 10% = 855
+	 */
+	if(operador.search('\\+|\\-') !== -1)
+		numero.innerHTML = resultadoAtual * (numero.textContent / 100)
+	/**
+	 * Quando *|/ o resultado é a extração da porcentagem ou o valor inverso da porcentagem respectivamente
+	 * exemplo: 950 * 10% = 95,  950 / 10% = 9500 (inverso de: 9500 * 10% = 950)
+	 */
+	else 
+		numero.innerHTML = (numero.textContent / 100);
+
+	igual();
+}
